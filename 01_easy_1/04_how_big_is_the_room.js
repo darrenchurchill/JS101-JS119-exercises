864b5352e83aa2f8a1ef728e97d46111859b200a
Darren Churchill <darrenmchurchill@gmail.com>
2023-02-10 09:29:00 -0800

    Take default units (meters/feet) as user input and calculate accordingly

--- 01_easy_1/04_how_big_is_the_room.js
+++ 01_easy_1/04_how_big_is_the_room.js
@@ -12,25 +12,39 @@
  */
 
 const readline = require('readline-sync');
+const SQFT_TO_SQM = 10.7639;
+const VALID_UNITS = {
+  meters: {conversionFn: sqFt, altUnits: 'feet'},
+  feet: {conversionFn: sqM, altUnits: 'meters'},
+};
 
-function sqft(squareMeters) {
-  return squareMeters * 10.7639;
+function sqFt(squareMeters) {
+  return squareMeters * SQFT_TO_SQM;
 }
 
-function promptRoomLength() {
-  console.log("Enter the length of the room in meters:");
+function sqM(squareFeet) {
+  return squareFeet / SQFT_TO_SQM;
+}
+
+function promptRoomLength(units) {
+  console.log(`Enter the length of the room in ${units}:`);
   return Number(readline.prompt());
 }
 
-function promptRoomWidth() {
-  console.log("Enter the width of the room in meters:");
+function promptRoomWidth(units) {
+  console.log(`Enter the width of the room in ${units}:`);
   return Number(readline.prompt());
 }
 
-let length = promptRoomLength();
-let width = promptRoomWidth();
-let sqMeters = length * width;
+console.log(`Enter the unit type: [${Object.keys(VALID_UNITS).join("/")}]`);
+let units = readline.prompt({limit: Object.keys(VALID_UNITS)}).toLowerCase();
+
+let length = promptRoomLength(units);
+let width = promptRoomWidth(units);
+let defaultArea = length * width;
+let convertedArea = VALID_UNITS[units].conversionFn(defaultArea);
+
 console.log(
-  `The area of the room is ${sqMeters.toFixed(2)} square meters `
-  + `(${sqft(sqMeters).toFixed(2)} square feet).`
+  `The area of the room is ${defaultArea.toFixed(2)} square ${units} `
+  + `(${convertedArea.toFixed(2)} square ${VALID_UNITS[units].altUnits}).`
 );
