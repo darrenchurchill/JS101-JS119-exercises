e0c8f1ea211e7cd0c3cb10cc96e7645e4db32ed5
Darren Churchill <darrenmchurchill@gmail.com>
2023-02-09 16:07:33 -0800

    Add Easy 1: Even Numbers

---
+++ 01_easy_1/03_even_numbers.js
@@ -0,0 +1,16 @@
+/**
+ * Darren Churchill
+ *
+ * Launch School Exercises
+ * JS101-JS119 - Small Problems
+ * Easy 1
+ *
+ * Even Numbers
+ *
+ * https://launchschool.com/exercises/29cee426
+ *
+ */
+
+for (let i = 2; i <= 99; i += 2) {
+  console.log(i);
+}
