7e649c0dd22228d2e8c97d8e935a7f2300116bd8
Darren Churchill <darrenmchurchill@gmail.com>
2023-02-14 15:58:35 -0800

    Add Easy 2: How Old is Teddy?

---
+++ 02_easy_2/09_how_old_is_teddy.js
@@ -0,0 +1,18 @@
+/**
+ * Darren Churchill
+ *
+ * Launch School Exercises
+ * JS101-JS119 - Small Problems
+ * Easy 2
+ *
+ * How Old is Teddy?
+ *
+ * https://launchschool.com/exercises/69a5b76d
+ *
+ */
+
+function getRandomAge() {
+  return 20 + Math.floor(Math.random() * 101);
+}
+
+console.log(`Teddy is ${getRandomAge()} years old!`);
