fe796ac8a9fe396301a1562b7ebacd3b5cbaf120
Darren Churchill <darrenmchurchill@gmail.com>
2023-02-14 12:48:31 -0800

    Add Easy 2: Multiplying Two Numbers

---
+++ 02_easy_2/03_multiplying_two_numbers.js
@@ -0,0 +1,18 @@
+/**
+ * Darren Churchill
+ *
+ * Launch School Exercises
+ * JS101-JS119 - Small Problems
+ * Easy 2
+ *
+ * Multiplying Two Numbers
+ *
+ * https://launchschool.com/exercises/8532d178
+ *
+ */
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+console.log(multiply(5, 3) === 15);
