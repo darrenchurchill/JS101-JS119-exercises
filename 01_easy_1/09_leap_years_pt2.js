c00faef83a02b9f1ea3fdf2fd2e62e238ce603df
Darren Churchill <darrenmchurchill@gmail.com>
2023-02-14 10:46:28 -0800

    Add Easy 1: Leap Years (Part 2)

---
+++ 01_easy_1/09_leap_years_pt2.js
@@ -0,0 +1,37 @@
+/**
+ * Darren Churchill
+ *
+ * Launch School Exercises
+ * JS101-JS119 - Small Problems
+ * Easy 1
+ *
+ * Leap Years (Part 2)
+ *
+ * This is a continuation of the previous exercise.
+ *
+ * https://launchschool.com/exercises/bd767ad6
+ *
+ */
+
+function isLeapYear(year) {
+  if (year <= 0) return false;
+  if (year <= 1752) return year % 4 === 0;
+  if (year % 400 === 0) return true;
+  if (year % 100 === 0) return false;
+  if (year % 4 === 0) return true;
+  return false;
+}
+
+console.log(isLeapYear(2016) === true);
+console.log(isLeapYear(2015) === false);
+console.log(isLeapYear(2100) === false);
+console.log(isLeapYear(2400) === true);
+console.log(isLeapYear(240000) === true);
+console.log(isLeapYear(240001) === false);
+console.log(isLeapYear(2000) === true);
+console.log(isLeapYear(1900) === false);
+console.log(isLeapYear(1752) === true);
+console.log(isLeapYear(1700) === true);
+console.log(isLeapYear(1) === false);
+console.log(isLeapYear(100) === true);
+console.log(isLeapYear(400) === true);
