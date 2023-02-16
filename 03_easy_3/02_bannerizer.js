9773d50895c2b1c9b34f9534fecbb9bed8df4a85
Darren Churchill <darrenmchurchill@gmail.com>
2023-02-15 20:01:47 -0800

    Further exploration: add rudimentary line-wrapping functionality
    This doesn't split lines at word breaks, so words are often broken apart.

--- 03_easy_3/02_bannerizer.js
+++ 03_easy_3/02_bannerizer.js
@@ -11,19 +11,46 @@
  *
  */
 
-function wrapInBox(text, maxBannerWidth = null) {
+function wrapInBox(text, maxBannerWidth = null, handleLongText = 'trunc') {
   const PADDING_TEXT = '| ';
   const BANNER_WIDTH = maxBannerWidth === null ?
     text.length + (PADDING_TEXT.length * 2) :
     Math.max(maxBannerWidth, PADDING_TEXT.length * 2);
 
-  text = genTruncatedText(text, PADDING_TEXT, BANNER_WIDTH);
+  let lines = [];
+
+  if (handleLongText === 'trunc') {
+    lines = [genTruncatedText(text, PADDING_TEXT, BANNER_WIDTH)];
+  } else if (handleLongText === 'wrap') {
+    lines = genWrappedLines(text, BANNER_WIDTH - (PADDING_TEXT.length * 2));
+  }
 
   return `${genTopBottom(BANNER_WIDTH)}\n`
        + `${genEmptyLine(BANNER_WIDTH, PADDING_TEXT)}\n`
-       + `${genLine(text, PADDING_TEXT)}\n`
+       + lines.reduce(
+           (accum, line) => accum + genLine(line, PADDING_TEXT) + '\n',
+           ''
+         )
        + `${genEmptyLine(BANNER_WIDTH, PADDING_TEXT)}\n`
        + `${genTopBottom(BANNER_WIDTH)}`;
+}
+
+function genWrappedLines(text, maxTextWidth) {
+  let result = [text.slice(0, maxTextWidth)];
+
+  for (let i = maxTextWidth; i <= text.length; i += maxTextWidth) {
+    // TODO: break lines at whole words instead of in random places.
+    result.push(text.slice(i, i + maxTextWidth));
+  }
+
+  if (result[result.length - 1].length < maxTextWidth) {
+    result[result.length - 1] = result[result.length - 1].padEnd(
+      maxTextWidth,
+      ' '
+    );
+  }
+
+  return result;
 }
 
 function genTruncatedText(text, paddingText = '| ', maxBannerWidth) {
@@ -50,12 +77,13 @@ function genTopBottom(bannerWidth, paddingText = '+') {
   return genLine('-'.repeat(bannerWidth - (paddingText.length * 2)), paddingText);
 }
 
-function logInBox(text, maxBannerWidth = null) {
-  console.log(wrapInBox(text, maxBannerWidth));
+function logInBox(text, maxBannerWidth = null, handleLongText = 'trunc') {
+  console.log(wrapInBox(text, maxBannerWidth, handleLongText));
 }
 
 logInBox('To boldly go where no one has gone before.');
 logInBox('To boldly go where no one has gone before.', 40);
+logInBox('To boldly go where no one has gone before.', 40, 'wrap');
 logInBox('To boldly go where no one has gone before.', 4);
 logInBox('To boldly go where no one has gone before.', 3);
 logInBox('');
