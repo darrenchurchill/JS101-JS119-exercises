c6706dc2bf2d4773ab4defb26e865891a707fc8d
Darren Churchill <darrenmchurchill@gmail.com>
2023-02-15 18:53:36 -0800

    Further exploration: truncate lines that are longer than a max width

--- 03_easy_3/02_bannerizer.js
+++ 03_easy_3/02_bannerizer.js
@@ -11,15 +11,29 @@
  *
  */
 
-function wrapInBox(text) {
+function wrapInBox(text, maxWidth = null) {
   const PADDING_TEXT = '| ';
-  const WIDTH = text.length + (PADDING_TEXT.length * 2);
+  const WIDTH = maxWidth === null ?
+    text.length + (PADDING_TEXT.length * 2) :
+    Math.max(maxWidth, PADDING_TEXT.length * 2);
+
+  text = genTruncatedText(text, PADDING_TEXT, WIDTH);
 
   return genTopBottom(WIDTH) + '\n'
          + genEmptyLine(WIDTH, PADDING_TEXT) + '\n'
          + genLine(text, PADDING_TEXT) + '\n'
          + genEmptyLine(WIDTH, PADDING_TEXT) + '\n'
          + genTopBottom(WIDTH);
+}
+
+function genTruncatedText(text, paddingText = '| ', maxWidth) {
+  const ELLIPSIS = '...';
+  if (text.length <= maxWidth - (paddingText.length * 2)) return text;
+  if (ELLIPSIS.length + 1 < maxWidth - (paddingText.length * 2)) {
+    return text.slice(0, maxWidth - (paddingText.length * 2) - ELLIPSIS.length)
+         + ELLIPSIS;
+  }
+  return '';
 }
 
 function genLine(bodyText, paddingText) {
@@ -34,9 +48,12 @@ function genTopBottom(width, paddingText = '+') {
   return genLine('-'.repeat(width - (paddingText.length * 2)), paddingText);
 }
 
-function logInBox(text) {
-  console.log(wrapInBox(text));
+function logInBox(text, maxWidth = null) {
+  console.log(wrapInBox(text, maxWidth));
 }
 
 logInBox('To boldly go where no one has gone before.');
+logInBox('To boldly go where no one has gone before.', 40);
+logInBox('To boldly go where no one has gone before.', 4);
+logInBox('To boldly go where no one has gone before.', 3);
 logInBox('');
